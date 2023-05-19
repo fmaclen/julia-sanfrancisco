24bfa83a2d977715e872106fa213702a06eeb3bf
Fernando Maclen <fmaclen@gmail.com>
2023-05-19 08:33:52 -0400

    feat: add typesafe-i18n

---
+++ src/i18n/es/index.ts
@@ -0,0 +1,8 @@
+import type { Translation } from '../i18n-types';
+
+const es = {
+	// Esta es una traducción de ejemplo, simplemente cambia el nombre o elimina esta carpeta si lo deseas
+	HI: '¡Hola {name}! Por favor, deja una estrella si te gusta el proyecto: https://github.com/ivanhofer/typesafe-i18n'
+} satisfies Translation;
+
+export default es;
