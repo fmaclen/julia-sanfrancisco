24e4e58eb0f399daf7cf865db8b5cdfee899c62c
Fernando Maclen <fmaclen@gmail.com>
2023-10-15 16:48:33 -0400

    Revert "Update app.d.ts"
    This reverts commit 51619a4f95b15ff5ce3a6b7eaf744c8e7fd77e4a.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,11 @@
-// REF https://kit.svelte.dev/docs/adapter-cloudflare#deployment
-
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
 declare global {
 	namespace App {
-		interface Platform {
-			env?: {
-				PUBLIC_PLAUSIBLE_DOMAIN: PublicPlausibleDomain;
-			};
-		}
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface Platform {}
 	}
 }
 
