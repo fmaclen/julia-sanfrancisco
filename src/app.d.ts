45b3b919c4e1a454bd568449c8e22b67726cde95
Fernando Maclen <fmaclen@gmail.com>
2023-10-15 17:02:32 -0400

    Update app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,11 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
+		interface Platform {
+			env?: {
+				PUBLIC_PLAUSIBLE_DOMAIN: string;
+				PUBLIC_GAME_VERSION: string;
+			};
+		}
 	}
 }
 
